5492258db12c2a9db06960cbf0c7c349c37c33f0
lisandroman <lisandromansilla.dg@gmail.com>
2022-07-18 23:00:11 +0300

    Fix Css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
 import { useAuth0 } from "@auth0/auth0-react";
+import './App.css'
 
 function App() {
   const { isLoading, error } = useAuth0();
 
   return (
-    <main className="column">
+    <main className="main">
       <h3>Auth0 Login</h3>
       {error && <p>Authentication Error</p>}
       {!error && isLoading && <p>Loading...</p>}
--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,11 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
-
+import '../App.css'
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
     !isAuthenticated && (
-      <button onClick={() => loginWithRedirect()}>
+      <button 
+        className="signIn"
+        onClick={() => loginWithRedirect()}>
         Sign In
       </button>
     )
